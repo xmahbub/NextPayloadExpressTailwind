3da30b1f8bdcd8ec3e5298ed61043cd6c61d6eb0
Mahbub Hasan <38033407+xmahbub@users.noreply.github.com>
2022-07-02 00:18:53 +0600

    e
    *

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function about() {
   return (
-    <div>This is about</div>
+    <button>Button</button>
+
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div class="tabs">
+  <a class="tab tab-lifted">Tab 1</a> 
+  <a class="tab tab-lifted tab-active">Tab 2</a> 
+  <a class="tab tab-lifted">Tab 3</a>
+</div>
   )
 }
